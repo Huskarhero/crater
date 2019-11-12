ad1ee23125e18fb407516515c33a8e99edb212b4
Huskarhero <stevenjobs207@gmail.com>
2019-11-12 18:58:19 +0530

    change translation message

--- database/seeds/EstimateTemplateSeeder.php
+++ database/seeds/EstimateTemplateSeeder.php
@@ -13,19 +13,19 @@ class EstimateTemplateSeeder extends Seeder
     public function run()
     {
         EstimateTemplate::create([
-            'name' => 'Template 1',
+            'name' => 'Estimate Template1',
             'view' => 'estimate1',
             'path' => '/assets/img/PDF/Template1.png'
         ]);
 
         EstimateTemplate::create([
-            'name' => 'Template 2',
+            'name' => 'Estimate Template2',
             'view' => 'estimate2',
             'path' => '/assets/img/PDF/Template2.png'
         ]);
 
         EstimateTemplate::create([
-            'name' => 'Template 3',
+            'name' => 'Estimate Template3',
             'view' => 'estimate3',
             'path' => '/assets/img/PDF/Template3.png'
         ]);
--- database/seeds/InvoiceTemplateSeeder.php
+++ database/seeds/InvoiceTemplateSeeder.php
@@ -13,19 +13,19 @@ class InvoiceTemplateSeeder extends Seeder
     public function run()
     {
         InvoiceTemplate::create([
-            'name' => 'Template 1',
+            'name' => 'Invoice Template1',
             'view' => 'invoice1',
             'path' => '/assets/img/PDF/Template1.png'
         ]);
 
         InvoiceTemplate::create([
-            'name' => ' Template 2',
+            'name' => 'Invoice Template2',
             'view' => 'invoice2',
             'path' => '/assets/img/PDF/Template2.png'
         ]);
 
         InvoiceTemplate::create([
-            'name' => 'Template 3',
+            'name' => 'Invoice Template3',
             'view' => 'invoice3',
             'path' => '/assets/img/PDF/Template3.png'
         ]);
