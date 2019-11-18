62526bd0c0f3b19a0121cfca131036dfd3dc325b
Huskarhero <stevenjobs207@gmail.com>
2019-11-18 19:41:31 +0530

    refactor dashboard actions and send email actions

--- app/Http/Controllers/EstimatesController.php
+++ app/Http/Controllers/EstimatesController.php
@@ -13,7 +13,6 @@ use Crater\User;
 use Crater\Item;
 use Validator;
 use Crater\CompanySetting;
-use Crater\Company;
 use Crater\Mail\EstimatePdf;
 use Crater\TaxType;
 use Crater\Tax;
@@ -146,11 +145,8 @@ class EstimatesController extends Controller
 
         if ($request->has('estimateSend')) {
             $data['estimate'] = $estimate->toArray();
-            $company = Company::find($data['estimate']['company_id']);
             $userId = $data['estimate']['user_id'];
             $data['user'] = User::find($userId)->toArray();
-            $data['logo'] = $company->getMedia('logo')->first();
-            $data['company_name'] = $company->name;
             $email = $data['user']['email'];
             $notificationEmail = CompanySetting::getSetting(
                 'notification_email',
@@ -310,14 +306,10 @@ class EstimatesController extends Controller
     public function sendEstimate(Request $request)
     {
         $estimate = Estimate::findOrFail($request->id);
-        $company = Company::find($estimate->company_id);
 
         $data['estimate'] = $estimate->toArray();
         $userId = $data['estimate']['user_id'];
         $data['user'] = User::find($userId)->toArray();
-        $data['logo'] = $company->getMedia('logo')->first();
-        $data['company_name'] = $company->name;
-
         $email = $data['user']['email'];
         $notificationEmail = CompanySetting::getSetting(
             'notification_email',
--- resources/views/emails/send/invoice.blade.php
+++ resources/views/emails/send/invoice.blade.php
@@ -1,11 +1,10 @@
 @component('mail::message')
-@if (! empty($logo))
-# {{ $company }}
-@else
+Thanks for buying.
+
 @component('mail::button', ['url' => url('/customer/invoices/pdf/'.$data['invoice']['unique_hash'])])
 Get your invoice
 @endcomponent
 
-{{-- Thanks,<br>
-{{ config('app.name') }} --}}
+Thanks,<br>
+{{ config('app.name') }}
 @endcomponent
