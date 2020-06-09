427255251257efc19f9fe49c04cd9c85a58bec6e
Huskarhero <stevenjobs207@gmail.com>
2020-06-09 09:34:56 -0400

    Added last slash so it ignores everything in the folder

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -150,8 +150,6 @@ class CompanyController extends Controller
             ["code"=>"de", "name" => "German"],
             ["code"=>"it", "name" => "Italian"],
             ["code"=>"es", "name" => "Spanish"],
-            ["code"=>"sr_LA", "name" => "Srpski"],
-            ["code"=>"sr_CY", "name" => "Српски"],
             ["code"=>"ar", "name" => "العربية"],
         ];
 
--- resources/assets/js/plugins/i18n.js
+++ resources/assets/js/plugins/i18n.js
@@ -7,8 +7,6 @@ import ar from './ar.json'
 import de from './de.json'
 import pt_BR from './pt-br.json'
 import it from './it.json'
-import sr_LA from './sr-latn.json'
-import sr_CY from './sr-cyrl.json'
 
 Vue.use(VueI18n)
 
@@ -22,8 +20,6 @@ const i18n = new VueI18n({
     de,
     pt_BR,
     it,
-    sr_LA, // Serbian Latin
-    sr_CY, // Serbian Cyrilic
   },
 })
 
