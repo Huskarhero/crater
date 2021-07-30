eaa441698dd803c9efd64e7b90cf40b1c4921bbd
Huskarhero <stevenjobs207@gmail.com>
2021-07-30 19:08:07 +0530

    fix partial payment issue

--- resources/assets/js/views/estimates/ItemSelect.vue
+++ resources/assets/js/views/estimates/ItemSelect.vue
@@ -104,6 +104,15 @@ export default {
       loading: false,
     }
   },
+  validations() {
+    return {
+      item: {
+        description: {
+          maxLength: maxLength(255),
+        },
+      },
+    }
+  },
   computed: {
     ...mapGetters('item', ['items']),
   },
