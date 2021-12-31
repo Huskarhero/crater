7a77a713987133c35ed9baa296c46d75a5406ffe
Huskarhero <stevenjobs207@gmail.com>
2021-12-31 15:35:14 +0530

    downgrade dompdf issue

--- tests/Unit/Request/ExpenseTest.php
+++ tests/Unit/Request/ExpenseTest.php
@@ -31,12 +31,6 @@ test('expense request validation rules', function () {
             'currency_id' => [
                 'required'
             ],
-            'attachment_receipt' => [
-                'nullable',
-                'file',
-                'mimes:jpg,png,pdf,doc,docx,xls,xlsx,ppt,pptx',
-                'max:20000'
-            ]
         ],
         $request->rules()
     );
